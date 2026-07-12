50f517f5fbbd83904b4841511a53e8e8d8ea809c
Vladimir Riazantsev <vriazantsev@proton.me>
2026-07-12 12:02:33 +0200

    fix ChatReadDto serialization

--- DataTransferObjects/Chat/Read/ChatReadDto.cs
+++ DataTransferObjects/Chat/Read/ChatReadDto.cs
@@ -1,8 +1,7 @@
 namespace Calibr8Fit.Api.DataTransferObjects.Chat.Read
 {
-    public record ChatReadDto(Guid chatId, DateTime fromMessageSentAt)
-    {
-        public Guid ChatId { get; init; } = chatId;
-        public DateTime FromMessageSentAt { get; init; } = fromMessageSentAt;
-    }
+    public record ChatReadDto(
+        Guid ChatId,
+        DateTime FromMessageSentAt
+    );
 }
